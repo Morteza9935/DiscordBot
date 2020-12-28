762521f35c53f4f969d25e0cb7f430104163aadf
Morteza9935 <76703590+Morteza9935@users.noreply.github.com>
2020-12-28 22:39:30 +0330

    Update verify-config.js

--- src/scripts/verify-config.js
+++ src/scripts/verify-config.js
@@ -59,33 +59,6 @@ module.exports.run = async (config) => {
 		});
 	}
 
-	// Check Ksoft API
-	if (!config.api_keys.ksoft) {
-		logger.error(`${chalk.red('✗')} Ksoft API key is missing.`);
-		error = true;
-	} else {
-		const ksoft = new KSoftClient(config.api_keys.ksoft);
-		const resp = await ksoft.images.meme();
-		if (!resp.url) {
-			logger.error(`${chalk.red('✗')} Ksoft API key is incorrect.`);
-			error = true;
-		}
-	}
-
-	// Check Steam API
-	if (!config.api_keys.steam) {
-		logger.error(`${chalk.red('✗')} Steam API key is missing.`);
-		error = true;
-	} else {
-		try {
-			await fetch(`http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${config.api_keys.steam}&vanityurl=eroticgaben`).then(res => res.json());
-		} catch (e) {
-			if (e.type == 'invalid-json') {
-				logger.error(`${chalk.red('✗')} Steam API key is incorrect.`);
-				error = true;
-			}
-		}
-	}
 
 	// Check for bot list API keys
 	if (config.DiscordBotLists) {
@@ -100,24 +73,6 @@ module.exports.run = async (config) => {
 		}
 		if (!config.DiscordBotLists.botlist_spaceAPI_KEY) {
 			logger.log(`${chalk.red('✓')} Botlist Space API key is missing.`);
-			error = true;
-		}
-	}
-
-	// Check Amethyste API
-	if (!config.api_keys.amethyste) {
-		logger.error(`${chalk.red('✗')} Amethyste API key is missing.`);
-		error = true;
-	} else {
-		const res = await fetch('https://v1.api.amethyste.moe/generate/blurple', {
-			method: 'POST',
-			headers: {
-				Authorization: `Bearer ${config.api_keys.amethyste}`,
-			},
-		});
-		const result = await res.json();
-		if (result.status === 401) {
-			logger.error(`${chalk.red('✗')} Invalid Amethyste API key.`);
 			error = true;
 		}
 	}
